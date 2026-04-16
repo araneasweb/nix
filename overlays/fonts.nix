31f55f48644baacbd180124346389d1275abb949
aranea <anahita@golshani.ca>
2026-04-16 16:32:22 -0700

    updated hack nerd font ligatured to work with darwin

--- overlays/fonts.nix
+++ overlays/fonts.nix
@@ -10,15 +10,20 @@ _: prev: {
       hash = "sha256-haURxyxy0vWzkrXR8B65Lzi2wLjc34YiT3O4rv7BTKA=";
     };
 
+    dontConfigure = true;
+    dontBuild = true;
+
     installPhase = ''
       runHook preInstall
 
-      mkdir -p $out/share/fonts/truetype
-
-      cp "font/Hack Regular Nerd Font Complete Mono.ttf" $out/share/fonts/truetype/
-      cp "font/Hack Bold Nerd Font Complete Mono.ttf" $out/share/fonts/truetype/
-      cp "font/Hack Italic Nerd Font Complete Mono.ttf" $out/share/fonts/truetype/
-      cp "font/Hack Bold Italic Nerd Font Complete Mono.ttf" $out/share/fonts/truetype/
+      install -Dm444 "font/Hack Regular Nerd Font Complete Mono.ttf" \
+        "$out/share/fonts/truetype/Hack Regular Nerd Font Complete Mono.ttf"
+      install -Dm444 "font/Hack Bold Nerd Font Complete Mono.ttf" \
+        "$out/share/fonts/truetype/Hack Bold Nerd Font Complete Mono.ttf"
+      install -Dm444 "font/Hack Italic Nerd Font Complete Mono.ttf" \
+        "$out/share/fonts/truetype/Hack Italic Nerd Font Complete Mono.ttf"
+      install -Dm444 "font/Hack Bold Italic Nerd Font Complete Mono.ttf" \
+        "$out/share/fonts/truetype/Hack Bold Italic Nerd Font Complete Mono.ttf"
 
       runHook postInstall
     '';
@@ -27,7 +32,7 @@ _: prev: {
       description = "hack-font-ligature-nerd-font";
       homepage = "https://github.com/pyrho/hack-font-ligature-nerd-font";
       license = licenses.mit;
-      platforms = platforms.all;
+      platforms = platforms.unix;
     };
   };
 }
